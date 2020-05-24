421e0416114ab5c869ca48622a9692719c318716
Joey de Ruiter <contact@joeyderuiter.me>
2020-05-24 23:12:00 +0200

    More info btn work

--- src/app/pages/home/more-info-btn/more-info-btn.component.ts
+++ src/app/pages/home/more-info-btn/more-info-btn.component.ts
@@ -1,14 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { CubeService } from '../../../shared/services/cube.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { CubeService, CubeData, CubeDataState } from '../../../shared/services/cube.service';
+import { Subscription } from 'rxjs';
+import { LanguageService } from '../../../shared/services/language.service';
 
 @Component({
   selector: 'app-more-info-btn',
   templateUrl: './more-info-btn.component.html',
   styleUrls: ['./more-info-btn.component.scss']
 })
-export class MoreInfoBtnComponent implements OnInit {
+export class MoreInfoBtnComponent implements OnInit, OnDestroy {
 
-  constructor(private service: CubeService) { }
+  private readonly START_ANIMATION_WAIT_TIME = 100;
+  private readonly START_ANIMATION_TIME = 250;
+  private readonly ANIMATION_TIME = 100;
+
+  private format = '';
+  public projectName = '';
+
+  private transitioning = false;
+  public state: 'idle' | 'start-fadein' | 'fadein' | 'fadeout' | 'hidden' = 'hidden';
+
+  private cubeServiceSubscription: Subscription;
+  private langServiceSubscription: Subscription;
+
+  constructor(
+    private service: CubeService,
+    private langService: LanguageService
+  ) {
+    this.cubeServiceSubscription = this.service.onChange.subscribe(this.onCubeChange.bind(this));
+    this.langServiceSubscription = this.langService.languageObservable.subscribe(this.onLangChange.bind(this));
+  }
+
+  ngOnInit(): void {
+    this.startFadein();
+  }
+
+  ngOnDestroy(): void {
+    this.cubeServiceSubscription.unsubscribe();
+    this.langServiceSubscription.unsubscribe();
+  }
 
   public onFadeout() {
     this.service.fadeout();
@@ -18,7 +48,124 @@ export class MoreInfoBtnComponent implements OnInit {
     this.service.fadein();
   }
 
-  ngOnInit(): void {
+  public onClick() {
+    if (this.service.currentState !== CubeDataState.NORMAL) { return; }
+
+    this.service.fadeout();
+  }
+
+  private onCubeChange(cubeData: CubeData) {
+    switch (cubeData.state) {
+      case CubeDataState.FADEOUT:
+        this.fadeout();
+        break;
+
+      case CubeDataState.FADEIN:
+        this.fadein();
+        break;
+
+      case CubeDataState.NORMAL:
+        this.reload();
+        break;
+    }
+  }
+
+  private onLangChange(lang: string) {
+    if (lang.startsWith('en')) {
+      this.format = 'More information about ';
+    } else if (lang.startsWith('nl')) {
+      this.format = 'Meer informatie over ';
+    }
+
+    this.updateProjectName();
+
+    if (this.state === 'idle' || this.state === 'fadein') {
+      this.reload();
+    }
+  }
+
+  private updateProjectName() {
+    this.projectName = this.format + this.service.currentName; 
+  }
+
+  private reload(): void {
+    if (!this.canTransition()) { return; }
+    this.startTransition();
+
+    this.state = 'fadeout';
+
+    setTimeout(() => {
+      this.updateProjectName();
+      this.state = 'fadein';
+
+
+      setTimeout(() => {
+        this.showButton();
+      }, this.ANIMATION_TIME);
+    }, this.ANIMATION_TIME);
+
+    this.endTransition();
+  }
+
+  private fadeout(): void {
+    if (!this.canTransition()) { return; }
+    this.startTransition();
+
+    this.state = 'fadeout';
+
+    setTimeout(() => {
+      this.hideButton();
+      this.endTransition();
+    }, this.ANIMATION_TIME);
+  }
+
+  private startFadein(): void {
+    if (!this.canTransition()) { return; }
+    this.startTransition();
+
+    // Every animation has a 100ms wait time
+    setTimeout(() => {
+
+      this.state = 'start-fadein';
+
+      setTimeout(() => {
+        this.showButton();
+        this.endTransition();
+      }, this.START_ANIMATION_TIME);
+    }, this.START_ANIMATION_WAIT_TIME);
+
+  }
+
+  private fadein(): void {
+    if (!this.canTransition()) { return; }
+    this.startTransition();
+
+    this.state = 'fadein';
+
+    setTimeout(() => {
+      this.showButton();
+      this.endTransition();
+    }, this.ANIMATION_TIME);
+  }
+
+  private hideButton(): void {
+    this.state = 'hidden';
+  }
+
+  private showButton(): void {
+    this.state = 'idle';
+  }
+
+  private canTransition(): boolean {
+    return this.transitioning === false;
+  }
+
+  private startTransition(): void {
+    this.transitioning = true;
+  }
+
+  private endTransition(): void {
+    this.transitioning = false;
   }
 
 }
